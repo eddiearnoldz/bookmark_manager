77c5b34d6e445eadada2867f44a4c46b3c6039e8
Eddie Arnold <eddiearnoldz@me.com>
2022-02-16 12:39:20 +0000

    added update functionality

--- app.rb
+++ app.rb
@@ -29,6 +29,16 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
   # Start the server if this file is executed directly 
   # (do not change the line below)
   run! if app_file == $0
